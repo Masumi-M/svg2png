8349343213af5a46ebae3b74d4824ec90c33077d
Masumi Morishige <masumi.developer@gmail.com>
2021-01-02 22:01:42 +0900

    Add python file

---
+++ svg2png.py
@@ -0,0 +1,13 @@
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF, renderPM
+import sys
+import os
+
+args = sys.argv
+filename = args[1]
+filename_without_ext = os.path.splitext(os.path.basename(filename))[0]
+
+drawing = svg2rlg(filename)
+renderPDF.drawToFile(drawing, filename_without_ext + ".pdf")
+drawing = svg2rlg(filename)
+renderPM.drawToFile(drawing, filename_without_ext + ".png", fmt="PNG")
